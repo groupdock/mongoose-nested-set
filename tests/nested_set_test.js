c1cab04af70c9333160f2fe6508ac1a1a7fea482
Luc Castera <luc.castera@gmail.com>
2011-12-28 16:14:40 -0500

    Added buildTree method to plugin

--- tests/nested_set_test.js
+++ tests/nested_set_test.js
@@ -4,32 +4,33 @@ var testCase = require('nodeunit').testCase,
     mongoose = require('mongoose'),
     NestedSetPlugin = require('../lib/nested_set'),
     Schema = mongoose.Schema,
-    Step = require('step'),
+    async = require('async'),
     Helpers = require('./helpers');
 
 var UserSchema,
     User
 
 var tests = testCase({
-  setUp: function(callback) {
-    Step(
-      function() {
+  setUp: function(next) {
+    async.series([
+      function(callback) {
         mongoose.connect('mongodb://localhost/nested_set_test');
-        this();
+        callback();
       },
-      function() {
+      function(callback) {
         UserSchema = new Schema({
           username: {type: String}
         });
         UserSchema.plugin(NestedSetPlugin);
         User = mongoose.model('User', UserSchema);
-        this();
+        callback();
       },
-      function() {
+      function(callback) {
         // drop users from mongodb
-        User.remove({}, this);
+        User.remove({}, callback);
       },
-      function() {
+      function(callback) {
+        // see diagram in docs/test_tree.png for a representation of this tree
         var michael = new User({username: 'michael'});
 
         var meredith = new User({username: 'meredith', parentId: michael._id});
@@ -45,22 +46,25 @@ var tests = testCase({
 
         var oscar = new User({username: 'oscar', parentId: angela._id});
 
-        michael.save(this.parallel());
-        meredith.save(this.parallel());
-        jim.save(this.parallel());
-        angela.save(this.parallel());
-        kelly.save(this.parallel());
-        creed.save(this.parallel());
-        phyllis.save(this.parallel());
-        stanley.save(this.parallel());
-        dwight.save(this.parallel());
-        oscar.save(this.parallel());
+        async.forEach([
+          michael,
+          meredith,
+          jim,
+          angela,
+          kelly,
+          creed,
+          phyllis,
+          stanley,
+          dwight,
+          oscar
+        ], function(item, cb) { item.save(cb) }, function(err) {
+          callback();
+        })
       },
-      function(err) {
-        if (err) { console.log(err); }
-        callback();
+      function(callback) {
+        next();
       }
-    )
+    ])
   },
   'is sane': function(test) {
     test.expect(3);
@@ -90,6 +94,49 @@ var tests = testCase({
         }
       });
       test.done();
+    });
+  },
+  'rebuildTree should set lft and rgt based on parentIds': function(test) {
+    User.findOne({username: 'michael'}, function(err, user) {
+      User.rebuildTree(user, 1, function() {
+        User.find(function(err, users) {
+          // see docs/test_tree.png for the graphical representation of this tree with lft/rgt values
+          users.forEach(function(person) {
+            if (person.username === 'michael') {
+              test.equal(1, person.lft);
+              test.equal(20, person.rgt);
+            } else if (person.username === 'meredith') {
+              test.equal(2, person.lft);
+              test.equal(7, person.rgt);
+            } else if (person.username === 'jim') {
+              test.equal(8, person.lft);
+              test.equal(15, person.rgt);
+            } else if (person.username === 'angela') {
+              test.equal(16, person.lft);
+              test.equal(19, person.rgt);
+            } else if (person.username === 'kelly') {
+              test.equal(3, person.lft);
+              test.equal(4, person.rgt);
+            } else if (person.username === 'creed') {
+              test.equal(5, person.lft);
+              test.equal(6, person.rgt);
+            } else if (person.username === 'phyllis') {
+              test.equal(9, person.lft);
+              test.equal(10, person.rgt);
+            } else if (person.username === 'stanley') {
+              test.equal(11, person.lft);
+              test.equal(12, person.rgt);
+            } else if (person.username === 'dwight') {
+              test.equal(13, person.lft);
+              test.equal(14, person.rgt);
+            } else if (person.username === 'oscar') {
+              test.equal(17, person.lft);
+              test.equal(18, person.rgt);
+            }
+          })
+          test.done();
+        })
+      });
     });
   }
 });
