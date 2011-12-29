055d41ce465e8e212be2b1f5f6fb00b964e92b8a
Luc Castera <luc.castera@gmail.com>
2011-12-29 16:40:46 -0500

    Added all helper methods

--- tests/nested_set_test.js
+++ tests/nested_set_test.js
@@ -58,7 +58,7 @@ var tests = testCase({
           dwight,
           oscar
         ], function(item, cb) { item.save(cb) }, function(err) {
-          callback();
+          setTimeout(callback, 500);
         })
       },
       function(callback) {
@@ -133,9 +133,215 @@ var tests = testCase({
               test.equal(17, person.lft);
               test.equal(18, person.rgt);
             }
-          })
+          });
           test.done();
-        })
+        });
+      });
+    });
+  },
+  'isLeaf should return true if node is leaf': function(test) {
+    test.expect(2)
+    User.findOne({username: 'michael'}, function(err, user) {
+      User.rebuildTree(user, 1, function() {
+        User.findOne({username: 'kelly'}, function(err, kelly) {
+          test.ok(kelly.isLeaf());
+          User.findOne({username: 'michael'}, function(err, michael) {
+            test.ok(!michael.isLeaf());
+            test.done();
+          });
+        });
+      });
+    });
+  },
+  'isChild should return true if node has a parent': function(test) {
+    test.expect(2)
+    User.findOne({username: 'michael'}, function(err, user) {
+      User.rebuildTree(user, 1, function() {
+        User.findOne({username: 'kelly'}, function(err, kelly) {
+          test.ok(kelly.isChild());
+          User.findOne({username: 'michael'}, function(err, michael) {
+            test.ok(!michael.isChild());
+            test.done();
+          });
+        });
+      });
+    });
+  },
+  'selfAndAncestors should return all ancestors higher up in tree + current node': function(test) {
+    test.expect(2);
+    User.findOne({username: 'michael'}, function(err, user) {
+      User.rebuildTree(user, 1, function() {
+        User.findOne({username: 'kelly'}, function(err, kelly) {
+          kelly.selfAndAncestors(function(err, people) {
+            test.ok(!err);
+            test.deepEqual(['kelly', 'meredith', 'michael'], people.map(function(p) {return p.username}).sort());
+            test.done();
+          });
+        });
+      });
+    });
+  },
+  'ancestors should return all ancestors higher up in tree': function(test) {
+    test.expect(2);
+    User.findOne({username: 'michael'}, function(err, user) {
+      User.rebuildTree(user, 1, function() {
+        User.findOne({username: 'kelly'}, function(err, kelly) {
+          kelly.ancestors(function(err, people) {
+            test.ok(!err);
+            test.deepEqual(['meredith', 'michael'], people.map(function(p) {return p.username}).sort());
+            test.done();
+          });
+        });
+      });
+    });
+  },
+  'ancestors should return empty array if it is a root node': function(test) {
+    test.expect(2);
+    User.findOne({username: 'michael'}, function(err, user) {
+      User.rebuildTree(user, 1, function() {
+        User.findOne({username: 'michael'}, function(err, michael) {
+          michael.ancestors(function(err, people) {
+            test.ok(!err);
+            test.deepEqual([], people.map(function(p) {return p.username}).sort());
+            test.done();
+          });
+        });
+      });
+    });
+  },
+  'selfAndChildren should return all children + current node': function(test) {
+    test.expect(2);
+    User.findOne({username: 'michael'}, function(err, user) {
+      User.rebuildTree(user, 1, function() {
+        User.findOne({username: 'michael'}, function(err, michael) {
+          michael.selfAndChildren(function(err, people) {
+            test.ok(!err);
+            test.deepEqual(
+              ['angela', 'jim', 'meredith', 'michael'], 
+              people.map(function(p) {return p.username}).sort());
+            test.done();
+          });
+        });
+      });
+    });
+  },
+  'children should return all children': function(test) {
+    test.expect(2);
+    User.findOne({username: 'michael'}, function(err, user) {
+      User.rebuildTree(user, 1, function() {
+        User.findOne({username: 'michael'}, function(err, michael) {
+          michael.children(function(err, people) {
+            test.ok(!err);
+            test.deepEqual(['angela', 'jim', 'meredith'], people.map(function(p) {return p.username}).sort());
+            test.done();
+          });
+        });
+      });
+    });
+  },
+  'selfAndDescendants should return all descendants + current node': function(test) {
+    test.expect(2);
+    User.findOne({username: 'michael'}, function(err, user) {
+      User.rebuildTree(user, 1, function() {
+        User.findOne({username: 'michael'}, function(err, michael) {
+          michael.selfAndDescendants(function(err, people) {
+            test.ok(!err);
+            test.deepEqual(
+              ['angela', 'creed', 'dwight', 'jim', 'kelly', 'meredith', 'michael', 'oscar', 'phyllis', 'stanley'], 
+              people.map(function(p) {return p.username}).sort()
+            );
+            test.done();
+          });
+        });
+      });
+    });
+  },
+  'descendants should return all descendants': function(test) {
+    test.expect(2);
+    User.findOne({username: 'michael'}, function(err, user) {
+      User.rebuildTree(user, 1, function() {
+        User.findOne({username: 'michael'}, function(err, michael) {
+          michael.descendants(function(err, people) {
+            test.ok(!err);
+            test.deepEqual(
+              ['angela', 'creed', 'dwight', 'jim', 'kelly', 'meredith', 'oscar', 'phyllis', 'stanley'], 
+              people.map(function(p) {return p.username}).sort()
+            );
+            test.done();
+          });
+        });
+      });
+    });
+  },
+  'level should return 0 for root node': function(test) {
+    test.expect(2)
+    User.findOne({username: 'michael'}, function(err, user) {
+      User.rebuildTree(user, 1, function() {
+        User.findOne({username: 'michael'}, function(err, michael) {
+          michael.level(function(err, value) {
+            test.ok(!err);
+            test.equal(0, value);
+            test.done();
+          });
+        });
+      });
+    });
+  },
+  'selfAndSiblings should return all nodes with same parent node + current node': function(test) {
+    test.expect(2);
+    User.findOne({username: 'michael'}, function(err, user) {
+      User.rebuildTree(user, 1, function() {
+        User.findOne({username: 'meredith'}, function(err, meredith) {
+          meredith.selfAndSiblings(function(err, people) {
+            test.ok(!err);
+            test.deepEqual(
+              ['angela', 'jim', 'meredith'],
+              people.map(function(p) {return p.username}).sort()
+            );
+            test.done();
+          });
+        });
+      });
+    });
+  },
+  'siblings should return all nodes with same parent node': function(test) {
+    test.expect(2);
+    User.findOne({username: 'michael'}, function(err, user) {
+      User.rebuildTree(user, 1, function() {
+        User.findOne({username: 'meredith'}, function(err, meredith) {
+          meredith.siblings(function(err, people) {
+            test.ok(!err);
+            test.deepEqual(
+              ['angela', 'jim'],
+              people.map(function(p) {return p.username}).sort()
+            );
+            test.done();
+          });
+        });
+      });
+    });
+  },
+  'kelly is a descendant of michael': function(test) {
+    test.expect(2);
+    User.findOne({username: 'michael'}, function(err, michael) {
+      User.rebuildTree(michael, 1, function() {
+        User.findOne({username: 'kelly'}, function(err, kelly) {
+          test.ok(kelly.isDescendantOf(michael));
+          test.ok(!michael.isDescendantOf(kelly));
+          test.done();
+        });
+      });
+    });
+  },
+  'michael is an ancestor of kelly': function(test) {
+    test.expect(2);
+    User.findOne({username: 'michael'}, function(err, michael) {
+      User.rebuildTree(michael, 1, function() {
+        User.findOne({username: 'kelly'}, function(err, kelly) {
+          test.ok(michael.isAncestorOf(kelly));
+          test.ok(!kelly.isAncestorOf(michael));
+          test.done();
+        });
       });
     });
   }
